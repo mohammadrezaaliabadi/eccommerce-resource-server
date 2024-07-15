fd0a2d370a7e6058d4a7f54485ea7db6038a3057
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-15 04:20:28 +0330

    Init projects

---
+++ src/main/java/com/pureamorous/eccommerceresourceserver/EccommerceResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.pureamorous.eccommerceresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccommerceResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EccommerceResourceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pureamorous/eccommerceresourceserver/EccommerceResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pureamorous.eccommerceresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccommerceResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
