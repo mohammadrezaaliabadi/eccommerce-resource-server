1ea3e4edbc6089e2e572925e7312e7b4b3d29fe9
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-16 15:28:37 +0330

    Seeding dummy data into database tables with spring command line runner. close #14

--- src/main/java/com/pureamorous/eccommerceresourceserver/EccommerceResourceServerApplication.java
+++ src/main/java/com/pureamorous/eccommerceresourceserver/EccommerceResourceServerApplication.java
@@ -1,7 +1,19 @@
 package com.pureamorous.eccommerceresourceserver;
 
+import com.pureamorous.eccommerceresourceserver.model.Brand;
+import com.pureamorous.eccommerceresourceserver.model.Category;
+import com.pureamorous.eccommerceresourceserver.model.Product;
+import com.pureamorous.eccommerceresourceserver.repository.BrandRepository;
+import com.pureamorous.eccommerceresourceserver.repository.CategoryRepository;
+import com.pureamorous.eccommerceresourceserver.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.Date;
 
 @SpringBootApplication
 public class EccommerceResourceServerApplication {
@@ -10,4 +22,113 @@ public class EccommerceResourceServerApplication {
         SpringApplication.run(EccommerceResourceServerApplication.class, args);
     }
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Bean
+    CommandLineRunner seedDataBase(){
+        return args -> {
+            Brand[] brands = new Brand[] {new Brand("Addidas"), new Brand("Slazenger"), new Brand("Nike") };
+            brandRepository.saveAll(Arrays.asList(brands));
+
+            Category[] cats = { new Category("Running"), new Category("Tennis"), new Category("Basketball")  };
+            categoryRepository.saveAll(Arrays.asList(cats));
+
+            Product[] products = {
+
+                    new Product(new Category(2L), new Brand(3L), "A101","World Star",
+                            "Shoes for next century", 195.5,"shoe-1.jpg", true, 12, new Date(2022,8,11),
+                            new  Date(2022, 8,11)),
+
+
+                    new Product(new Category(1L), new Brand(3L), "A102","White Line",
+                            "Will make you world champion", 295.5,"shoe-2.jpg", true, 12, new
+                            Date(2022,8,11), new Date(2022, 8,11)),
+
+                    new Product(new Category(3L), new Brand(3L), "A103","Prism White",
+                            "You have already won a gold medal", 135.5,"shoe-3.jpg", true, 12, new
+                            Date(2022,8,11), new Date(2022, 8,11)),
+
+
+                    new Product(new Category(1L), new Brand(1L), "A104","Olympic Runner",
+                            "Hitect in shoesy", 195.5,"shoe-4.jpg", true, 12, new Date(2022,8,11), new
+                            Date(2022, 8,11)),
+
+
+                    new Product(new Category(2L), new Brand(2L), "A105","Lala Land Shoes",
+                            "Romantism with hitech combined", 185.5,"shoe-5.jpg", true, 12, new
+                            Date(2022,8,11), new Date(2022, 8,11)),
+
+
+                    new Product(new Category(3L), new Brand(3L), "A106","Dunker in the Sky",
+                            "Blue Star  for next century", 195.5,"shoe-6.jpg", true, 12, new
+                            Date(2022,8,11), new Date(2022, 8,11)),
+
+                    new Product(new Category(1L), new Brand(2L),
+                            "A107","Even the smallest can dunk", "Bouncy Shoes for next century",
+                            165.5,"shoe-7.jpg", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
+
+
+
+
+
+                    new Product(new Category(2L), new Brand(3L), "A108","Wimbledon Star",
+                            "Grass or sand don't matter", 167.5,"shoe-8.jpg", true, 12, new
+                            Date(2022,8,11), new Date(2022, 8,11)),
+
+                    new Product(new Category(3L), new Brand(1L),
+                            "A109","Will blow your brains out!", "Do something good for your brains",
+                            175.5,"shoe-9.jpg", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
+
+
+                    new Product(new Category(1L), new Brand(3L), "A110","Universal  Star",
+                            "Neptune Plazma", 105.5,"shoe-10.jpg", true, 12, new Date(2022,8,11), new
+                            Date(2022, 8,11)),
+
+
+                    new Product(new Category(2L), new Brand(2L), "A111","Saturn",
+                            "Will take you to the Saturn", 115.5,"shoe-11.jpg", true, 12, new
+                            Date(2022,8,11), new Date(2022, 8,11)),
+
+
+                    new Product(new Category(3L), new Brand(1L), "A112","Paris Blues",
+                            "Save the environment", 195.5,"shoe-12.jpg", true, 12, new Date(2022,8,11),
+                            new Date(2022, 8,11)), new Product(new Category(2L), new Brand(3L),
+                    "A113","Vegan  Star", "Vegan", 125.5,"shoe-13.jpg", true, 12, new
+                    Date(2022,8,11), new Date(2022, 8,11)),
+
+
+                    new Product(new Category(2L), new Brand(2L), "A114","London Star",
+                            "Piccadily", 145.5,"shoe-14.jpg", true, 12, new Date(2022,8,11), new
+                            Date(2022, 8,11)),
+
+                    new Product(new Category(1L), new Brand(1L), "A115","Istanbul Star",
+                            "Bhosphoros Blues", 165.5,"shoe-15.jpg", true, 12, new Date(2022,8,11), new
+                            Date(2022, 8,11)),
+
+
+                    new Product(new Category(3L), new Brand(3L), "A116","One and Only",
+                            "Roland Garros", 155.5,"shoe-16.jpg", true, 12, new Date(2022,8,11), new
+                            Date(2022, 8,11)),
+
+
+                    new Product(new Category(1L), new Brand(2L), "A117","NBA Star", "Warriors",
+                            125.5,"shoe-17.jpg", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
+
+
+                    new Product(new Category(2L), new Brand(3L), "A118","Atlantic All Star ",
+                            "Wimbledon", 191.5,"shoe-18.jpg", true, 12, new Date(2022,8,11), new
+                            Date(2022, 8,11)), };
+
+            productRepository.saveAll(Arrays.asList(products));
+
+
+        };
+    }
 }
