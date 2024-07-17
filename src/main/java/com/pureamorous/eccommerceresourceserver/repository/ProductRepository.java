f54c08140aeb90faa3a0ee8f5bd0e3e84a5b3614
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-18 00:33:23 +0330

    Update product repository for specification query and pageable. close #22

--- src/main/java/com/pureamorous/eccommerceresourceserver/repository/ProductRepository.java
+++ src/main/java/com/pureamorous/eccommerceresourceserver/repository/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.pureamorous.eccommerceresourceserver.repository;
 
 import com.pureamorous.eccommerceresourceserver.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
+
+    Page<Product> findAll(Specification<Product> spec,Pageable pageable);
+    List<Product> findAll(Specification<Product> spec);
 }
