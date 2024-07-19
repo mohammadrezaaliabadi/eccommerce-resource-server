405abf750659145cd878e0b80358d207d038530d
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-19 17:34:34 +0330

    Update spring security config and add cors config for angular standalone frontend. close #23

--- src/main/java/com/pureamorous/eccommerceresourceserver/config/ProjectSecurityConfig.java
+++ src/main/java/com/pureamorous/eccommerceresourceserver/config/ProjectSecurityConfig.java
@@ -1,16 +1,38 @@
 package com.pureamorous.eccommerceresourceserver.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     public SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .cors(corsCustomizer->corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                                config.setAllowedMethods(Collections.singletonList("*"));
+                                config.setAllowCredentials(true);
+                                config.setAllowedHeaders(Collections.singletonList("*"));
+                                config.setExposedHeaders(Arrays.asList("Authorization"));
+                                config.setMaxAge(3600L);
+                                return config;
+                            }
+                        }));
 
         http.authorizeHttpRequests((authorize) ->
                 authorize
