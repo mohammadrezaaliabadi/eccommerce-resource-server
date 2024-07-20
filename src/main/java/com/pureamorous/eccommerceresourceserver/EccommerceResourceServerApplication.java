fab0f442272210d20fc2747cac552bc292843d67
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-07-20 18:44:48 +0330

    Update product init data name. close #24

--- src/main/java/com/pureamorous/eccommerceresourceserver/EccommerceResourceServerApplication.java
+++ src/main/java/com/pureamorous/eccommerceresourceserver/EccommerceResourceServerApplication.java
@@ -43,87 +43,87 @@ public class EccommerceResourceServerApplication {
             Product[] products = {
 
                     new Product(new Category(2L), new Brand(3L), "A101","World Star",
-                            "Shoes for next century", 195.5,"shoe-1.jpg", true, 12, new Date(2022,8,11),
+                            "Shoes for next century", 195.5,"shoes-1.png", true, 12, new Date(2022,8,11),
                             new  Date(2022, 8,11)),
 
 
                     new Product(new Category(1L), new Brand(3L), "A102","White Line",
-                            "Will make you world champion", 295.5,"shoe-2.jpg", true, 12, new
+                            "Will make you world champion", 295.5,"shoes-2.png", true, 12, new
                             Date(2022,8,11), new Date(2022, 8,11)),
 
                     new Product(new Category(3L), new Brand(3L), "A103","Prism White",
-                            "You have already won a gold medal", 135.5,"shoe-3.jpg", true, 12, new
+                            "You have already won a gold medal", 135.5,"shoes-3.png", true, 12, new
                             Date(2022,8,11), new Date(2022, 8,11)),
 
 
                     new Product(new Category(1L), new Brand(1L), "A104","Olympic Runner",
-                            "Hitect in shoesy", 195.5,"shoe-4.jpg", true, 12, new Date(2022,8,11), new
+                            "Hitect in shoesy", 195.5,"shoes-4.png", true, 12, new Date(2022,8,11), new
                             Date(2022, 8,11)),
 
 
                     new Product(new Category(2L), new Brand(2L), "A105","Lala Land Shoes",
-                            "Romantism with hitech combined", 185.5,"shoe-5.jpg", true, 12, new
+                            "Romantism with hitech combined", 185.5,"shoes-5.png", true, 12, new
                             Date(2022,8,11), new Date(2022, 8,11)),
 
 
                     new Product(new Category(3L), new Brand(3L), "A106","Dunker in the Sky",
-                            "Blue Star  for next century", 195.5,"shoe-6.jpg", true, 12, new
+                            "Blue Star  for next century", 195.5,"shoes-6.png", true, 12, new
                             Date(2022,8,11), new Date(2022, 8,11)),
 
                     new Product(new Category(1L), new Brand(2L),
                             "A107","Even the smallest can dunk", "Bouncy Shoes for next century",
-                            165.5,"shoe-7.jpg", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
+                            165.5,"shoes-7.png", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
 
 
 
 
 
                     new Product(new Category(2L), new Brand(3L), "A108","Wimbledon Star",
-                            "Grass or sand don't matter", 167.5,"shoe-8.jpg", true, 12, new
+                            "Grass or sand don't matter", 167.5,"shoes-8.png", true, 12, new
                             Date(2022,8,11), new Date(2022, 8,11)),
 
                     new Product(new Category(3L), new Brand(1L),
                             "A109","Will blow your brains out!", "Do something good for your brains",
-                            175.5,"shoe-9.jpg", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
+                            175.5,"shoes-9.png", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
 
 
                     new Product(new Category(1L), new Brand(3L), "A110","Universal  Star",
-                            "Neptune Plazma", 105.5,"shoe-10.jpg", true, 12, new Date(2022,8,11), new
+                            "Neptune Plazma", 105.5,"shoes-10.png", true, 12, new Date(2022,8,11), new
                             Date(2022, 8,11)),
 
 
                     new Product(new Category(2L), new Brand(2L), "A111","Saturn",
-                            "Will take you to the Saturn", 115.5,"shoe-11.jpg", true, 12, new
+                            "Will take you to the Saturn", 115.5,"shoes-11.png", true, 12, new
                             Date(2022,8,11), new Date(2022, 8,11)),
 
 
                     new Product(new Category(3L), new Brand(1L), "A112","Paris Blues",
                             "Save the environment", 195.5,"shoe-12.jpg", true, 12, new Date(2022,8,11),
                             new Date(2022, 8,11)), new Product(new Category(2L), new Brand(3L),
-                    "A113","Vegan  Star", "Vegan", 125.5,"shoe-13.jpg", true, 12, new
+                    "A113","Vegan  Star", "Vegan", 125.5,"shoes-13.png", true, 12, new
                     Date(2022,8,11), new Date(2022, 8,11)),
 
 
                     new Product(new Category(2L), new Brand(2L), "A114","London Star",
-                            "Piccadily", 145.5,"shoe-14.jpg", true, 12, new Date(2022,8,11), new
+                            "Piccadily", 145.5,"shoes-14.png", true, 12, new Date(2022,8,11), new
                             Date(2022, 8,11)),
 
                     new Product(new Category(1L), new Brand(1L), "A115","Istanbul Star",
-                            "Bhosphoros Blues", 165.5,"shoe-15.jpg", true, 12, new Date(2022,8,11), new
+                            "Bhosphoros Blues", 165.5,"shoes-15.png", true, 12, new Date(2022,8,11), new
                             Date(2022, 8,11)),
 
 
                     new Product(new Category(3L), new Brand(3L), "A116","One and Only",
-                            "Roland Garros", 155.5,"shoe-16.jpg", true, 12, new Date(2022,8,11), new
+                            "Roland Garros", 155.5,"shoes-16.png", true, 12, new Date(2022,8,11), new
                             Date(2022, 8,11)),
 
 
                     new Product(new Category(1L), new Brand(2L), "A117","NBA Star", "Warriors",
-                            125.5,"shoe-17.jpg", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
+                            125.5,"shoes-17.png", true, 12, new Date(2022,8,11), new Date(2022, 8,11)),
 
 
                     new Product(new Category(2L), new Brand(3L), "A118","Atlantic All Star ",
-                            "Wimbledon", 191.5,"shoe-18.jpg", true, 12, new Date(2022,8,11), new
+                            "Wimbledon", 191.5,"shoes-18.png", true, 12, new Date(2022,8,11), new
                             Date(2022, 8,11)), };
 
             productRepository.saveAll(Arrays.asList(products));
